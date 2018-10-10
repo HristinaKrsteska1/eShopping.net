a5d38ef3570671430041a310f331e0ab2e4e3ae5
HristinaKrsteska1 <hristina.krsteska1@gmail.com>
2018-10-10 18:34:24 +0200

    added login

---
+++ eShopping.NET/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eShopping.NET.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return RedirectToAction("~/Account/login");
+        }
+
+        // GET: Account/Login
+        public ActionResult Login()
+        {
+            //Confirm that user is not logged in
+
+            string username = User.Identity.Name;
+
+            if (!string.IsNullOrEmpty(username))
+            {
+                return RedirectToAction("user-profile");
+            }
+            //Return view
+            return View();
+        }
+
+
+        // GET: /account/create-account
+        [ActionName("create-account")]
+        public ActionResult CreateAccount()
+        {
+            return View("CreateAccount");
+        }
+    }
+}
